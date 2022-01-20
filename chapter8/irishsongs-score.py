e9e3aaa321e10179ae87b07dd44ef84cd413f4b2
Manoj Bableshwar <mabables@microsoft.com>
2022-01-20 08:19:06 -0800

    azureml job

--- chapter8/irishsongs-score.py
+++ chapter8/irishsongs-score.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 
 import tensorflow as tf 
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -34,17 +35,19 @@ for line in corpus:
 # pad sequences 
 max_sequence_len = max([len(x) for x in input_sequences])
 
-score_data = open(os.path.join(args.score, os.listdir(args.score)[0])).read()
+score_data = open(os.path.join(args.score, os.listdir(args.score)[0])).read().lower().split("\n")
 next_words = int(args.predict_count)
 score_output = []
 
 print (score_data)
 
 for seed_text in score_data:
+	print(seed_text)
 	for _ in range(next_words):
 		token_list = tokenizer.texts_to_sequences([seed_text])[0]
 		token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-		predicted = model.predict_classes(token_list, verbose=0)
+		#predicted = model.predict_classes(token_list, verbose=0)
+		predicted = np.argmax(model.predict(token_list), axis=-1)
 		output_word = ""
 		for word, index in tokenizer.word_index.items():
 			if index == predicted:
